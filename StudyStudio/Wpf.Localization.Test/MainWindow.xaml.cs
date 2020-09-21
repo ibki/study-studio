2ee627b0d0088cf20f90a59b0ff7749d9f798071
kslim <kslim@tectone.co.kr>
2020-09-21 10:51:37 +0900

    Wpf.Localization.Test - Add korean CultureInfo button and main window display button

--- StudyStudio/Wpf.Localization.Test/MainWindow.xaml.cs
+++ StudyStudio/Wpf.Localization.Test/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Wpf.Localization.Test
 {
@@ -31,6 +19,12 @@ namespace Wpf.Localization.Test
             Properties.Resources.Culture = new CultureInfo((sender as Button).Tag?.ToString() ?? "");
 
             HelloWorldTextBlock.Text = Properties.Resources.HelloWorld;
+        }
+
+        private void ShowMainWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
         }
     }
 }
