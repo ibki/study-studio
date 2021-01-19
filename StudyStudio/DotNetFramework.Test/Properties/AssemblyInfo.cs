92c46e2a56347e830b052a21dacf5c91cb53a910
Lim Ki-Sung <mribki88@outlook.com>
2021-01-19 10:22:55 +0900

    Add .NET Framework Test

---
+++ StudyStudio/DotNetFramework.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetFramework.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetFramework.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e291e6f7-bbd7-4797-bb35-3c6c9505bd53")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StudyStudio/DotNetFramework.Test/TryParseTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace DotNetFramework.Test
+{
+    [TestClass]
+    public class TryParseTests
+    {
+        [TestMethod]
+        public void DoubleTryParse_ExponentialString()
+        {
+            // double ±5.0 x 10-324 ~ ±1.7 x 10+308
+
+            // Arrange
+            string minValue = "-1.7976931348623157E+308";
+            string maxValue = "1.7976931348623157E+308";
+            string epsilon = "4.94065645841247E-324";
+            string testValue = "1.5E+02";
+
+            // Act && Assert
+            Assert.IsTrue(double.TryParse(minValue, out double minDouble));
+            Assert.IsTrue(double.TryParse(maxValue, out double maxDouble));
+            Assert.IsTrue(double.TryParse(epsilon, out double epsilonDouble));
+            Assert.IsTrue(double.TryParse(testValue, out double testDouble));
+
+            Console.WriteLine(
+                $"{nameof(minDouble)} = {minDouble}\r\n\r\n" +
+                $"{nameof(maxDouble)} = {maxDouble}\r\n\r\n" +
+                $"{nameof(epsilonDouble)} = {epsilonDouble}\r\n\r\n" +
+                $"{nameof(testDouble)} = {testDouble}\r\n\r\n");
+
+            Console.Write(
+                $"{nameof(minDouble)} = {minDouble.ToString("0." + new string('#', 339))}\r\n\r\n" +
+                $"{nameof(maxDouble)} = {maxDouble.ToString("0." + new string('#', 339))}\r\n\r\n" +
+                $"{nameof(epsilonDouble)} = {epsilonDouble.ToString("0." + new string('#', 339))}\r\n\r\n" +
+                $"{nameof(testDouble)} = {testDouble.ToString("0." + new string('#', 339))}\r\n\r\n");
+        }
+    }
+}
