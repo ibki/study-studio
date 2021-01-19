756b7f33130b6c9d1c09428fdfdb8e49443644bd
Lim Ki-Sung <mribki88@outlook.com>
2021-01-19 10:54:33 +0900

    Add LongestPalindromicSubstringTests

--- StudyStudio/LeetCode.Test/LongestPalindromicSubstringTests.cs
+++ StudyStudio/LeetCode.Test/LongestPalindromicSubstringTests.cs
@@ -1,14 +1,56 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿/*
+
+https://leetcode.com/problems/longest-palindromic-substring/
+
+5. Longest Palindromic Substring
+
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+
+Example 3:
+Input: s = "a"
+Output: "a"
+
+Example 4:
+Input: s = "ac"
+Output: "a"
+
+Constraints:
+1 <= s.length <= 1000
+s consist of only digits and English letters (lower-case and/or upper-case),
+
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LeetCode.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class LongestPalindromicSubstringTests
     {
         [TestMethod]
-        public void TestMethod1()
+        [DataRow("babad", "bab")]
+        [DataRow("cbbd", "bb")]
+        [DataRow("a", "a")]
+        [DataRow("ac", "a")]
+        public void LongestPalindrome(string s, string output)
         {
+            var result = LongestPalindrome(s);
+
+            Assert.AreEqual(output, result);
+        }
+
+        public string LongestPalindrome(string s)
+        {
+            return null;
         }
     }
 }
